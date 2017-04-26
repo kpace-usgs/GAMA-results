10e39b219e14f9a98b9805790490726a0ea473bd
Kylie M Pace <kpace@usgs.gov>
2017-04-26 16:20:34 -0700

    initial commit

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="message"><h3>{{ msg }}</h3></div>
+</template>
+
+<script>
+export default {
+	name: 'App',
+	  data () {
+	    return {
+	      msg: 'Goodbye!'
+	    }
+	  }
+}
+</script>
+
+<style>
+.message {
+  color: blue;
+}
+</style>
